2c562a4032d4d498c6d855f4cb5176034413007a
Jack Bice <johnbice3@gmail.com>
2026-09-15 15:39:19 -0400

    Add return for clean validation errors from API

--- src/main/java/com/example/employee_management/GlobalExceptionHandler.java
+++ src/main/java/com/example/employee_management/GlobalExceptionHandler.java
@@ -1,8 +1,15 @@
 package com.example.employee_management;
+
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.validation.FieldError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import java.util.Map;
+import java.util.HashMap;
+
+
 
 
 @RestControllerAdvice
@@ -11,5 +18,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<String> employeeNotFoundExceptionHandler(EmployeeNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception){
+        Map<String, String> errorMessages = new HashMap<>();
+        
+        for (FieldError fieldError: exception.getBindingResult().getFieldErrors()){
+            errorMessages.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessages);
     }
 }
